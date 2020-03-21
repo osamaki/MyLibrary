98a84bac112d50e42c7d710f7295484000ec322e
osamaki <soresunawati@docomo.ne.jp>
2020-03-22 05:55:28 +0900

    second commit

---
+++ utils.py
@@ -0,0 +1,53 @@
+import types
+
+def is_japanese(string):
+    import unicodedata
+    for ch in string:
+        name = unicodedata.name(ch) 
+        if "CJK UNIFIED" in name \
+        or "HIRAGANA" in name \
+        or "KATAKANA" in name:
+            return True
+    return False
+
+def explain(item, shows_private=False, shows_method=False):
+    '''
+    与えた python オブジェクトの詳細を表示します。
+    '''
+    print('EXPLAIN ------------------')
+    print(item)
+    print(type(item))
+    print('ATTRIBUTES:')
+    for d in dir(item):
+        if d == 'type':
+            continue
+        if not shows_private and d.startswith('_'):
+            continue
+        attr = getattr(item, d)
+        if not shows_method and (
+                isinstance(attr, types.MethodType) or
+                isinstance(attr, types.BuiltinMethodType) or
+                isinstance(attr, types.CoroutineType) or
+                isinstance(attr, types.FunctionType) or
+                isinstance(attr, types.BuiltinFunctionType) or
+                isinstance(attr, types.GeneratorType)
+        ):
+            continue
+        print('{}:\t{}'.format(d, attr))
+
+#品詞と特徴のタプルのリストを返す
+def mecab_list(text):
+    tagger = MeCab.Tagger("-Ochasen")
+    tagger.parse('')
+    node = tagger.parseToNode(text)
+    word_class = []
+    while node:
+        word = node.surface
+        wclass = node.feature.split(',')
+        if wclass[0] != u'BOS/EOS':
+            if wclass[6] == None:
+                word_class.append((word,wclass[0],wclass[1],wclass[2],""))
+            else:
+                word_class.append((word,wclass[0],wclass[1],wclass[2],wclass[6]))
+        node = node.next
+    return word_class
